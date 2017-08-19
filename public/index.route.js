28db2f4a57d33aaea1ad5cd76f706cdf8b299738
mohsini172 <mohsini172@gmail.com>
2017-08-19 18:57:57 +0500

    basic routing and profile template is added

--- public/index.route.js
+++ public/index.route.js
@@ -1,7 +1,13 @@
 angular.module('myApp')
 .config(function($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "./views/profile.html"
+    .when("/user", {
+        templateUrl : "./views/user-profile.html"
+    })
+    .when("/company", {
+        templateUrl : "./views/company-profile.html"
+    })
+    .otherwise({
+        template : "<h1 style='max-width:1400px;margin:80px'>404 Page Not Found</h1>"
     });
 });
